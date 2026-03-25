f5c4fb180f3e8a7cf7553c66ff91ae00fcfeaf5b
Kevin <kewe1002@stud.hs-kl.de>
2026-03-25 12:17:19 +0100

    Initialize Spring Boot project with Maven.

---
+++ src/main/java/studienprojekt/geraeteverwaltung/GeraeteverwaltungApplication.java
@@ -0,0 +1,13 @@
+package studienprojekt.geraeteverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeraeteverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeraeteverwaltungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studienprojekt/geraeteverwaltung/GeraeteverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package studienprojekt.geraeteverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeraeteverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
